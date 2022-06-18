13f5aa6abcff14fb27d72d6dfab6e26bca50f34f
Javier R <javierresines@gmail.com>
2022-06-17 23:32:43 -0500

    working on tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,11 +36,45 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+
+  Tag.create({
+    
+    tag_name: req.body.tag_name
+  })
+  .then (tData => res.json (tData)).catch(err => { console.log(err), res.status(500).json(err);});
 });
 
+
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+Tag.update(req.body, {
+  where: {
+   id:req.params.id
+  }
+})
+.then(tData =>
+  {
+    if (!tData){
+      res.status(404).json({message: 'Tag not found'});
+
+      return;
+      
+
+    }
+  })
+
+
+
+})
+  
+
+
+
+
+
+
+
+
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
